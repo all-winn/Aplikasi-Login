776310fad9ec404c8d781a1e38a8a717faf96438
all-winn <92625916+all-winn@users.noreply.github.com>
2024-02-13 19:07:48 +0700

    Update login-script.js
    Mengganti komentar sesuai format JSDoc.

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,31 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/* Membuat variabel untuk setiap element */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* Membuat variabel untuk menyimpan informasi email dan password. */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* Menambahkan aksi klik pada button */
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+    /* Mendapatkan nilai dari masing-masing input ketika tombol ditekan*/
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value; 
+  
+    /* Memastikan nilai email dan password sesuai nilai yang tersimpan */
+    if (email == expectedEmail && password == expectedPassword){
+
+      /* Jika sesuai maka program akan berpindah ke halaman home */
+      goToHome();
+    } else {
+
+      /* Namun jika tidak sesuai maka menampilkan informasi input salah */
+      showPopUp();
+    }
 });
